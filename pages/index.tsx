30da5714bf7c80b8ffea0c2880bafca2825b9df3
adham618 <adhamtarek291@gmail.com>
2021-12-14 19:32:32 +0200

    add link component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
@@ -9,6 +10,9 @@ const Home: NextPage = () => {
       <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repudiandae eligendi ad similique quod id quis sapiente sint autem blanditiis error voluptates, eius, libero dolorum soluta inventore! Nostrum, obcaecati magni.</p>
+      <Link href="/ninjas">
+        <a> see Ninja Listing</a>
+      </Link>
       <Footer />
     </>
   )
