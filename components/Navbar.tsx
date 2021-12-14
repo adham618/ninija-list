6d6e43b9ca6254e45d34f71f7254ed903c195a42
adham618 <adhamtarek291@gmail.com>
2021-12-14 18:52:15 +0200

    create navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+interface NavbarProps {
+
+}
+
+const Navbar: React.FC<NavbarProps> = ({ }) => {
+  return (
+    <nav>
+      <div className="logo"></div>
+    </nav>
+  )
+}
+export default Navbar
