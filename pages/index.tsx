12cea9b5dbbac1342039bf4727d030695d89f25d
adham618 <adhamtarek291@gmail.com>
2021-12-15 16:02:44 +0200

    remove footer && navbar from homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repudiandae eligendi ad similique quod id quis sapiente sint autem blanditiis error voluptates, eius, libero dolorum soluta inventore! Nostrum, obcaecati magni.</p>
       <Link href="/ninjas">
         <a> see Ninja Listing</a>
       </Link>
-      <Footer />
     </>
   )
 }
