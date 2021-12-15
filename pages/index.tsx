6fb2803b9365fd3e7e5987f71c222f811ef788c9
adham618 <adhamtarek291@gmail.com>
2021-12-15 16:54:57 +0200

    style the content

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h1>Homepage</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repudiandae eligendi ad similique quod id quis sapiente sint autem blanditiis error voluptates, eius, libero dolorum soluta inventore! Nostrum, obcaecati magni.</p>
+    <div>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repudiandae eligendi ad similique quod id quis sapiente sint autem blanditiis error voluptates, eius, libero dolorum soluta inventore! Nostrum, obcaecati magni.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repudiandae eligendi ad similique quod id quis sapiente sint autem blanditiis error voluptates, eius, libero dolorum soluta inventore! Nostrum, obcaecati magni.</p>
       <Link href="/ninjas">
-        <a> see Ninja Listing</a>
+        <a className={styles.btn}> see Ninja Listing</a>
       </Link>
-    </>
+    </div>
   )
 }
 
