4d68d4ef680bb58ef0a951370370b42d50e22cdc
adham618 <adhamtarek291@gmail.com>
2021-12-15 16:09:32 +0200

    make a div with classname content

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,11 +3,11 @@ import Navbar from "./Navbar"
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <>
+    <div className="content">
       <Navbar />
       {children}
       <Footer />
-    </>
+    </div>
   )
 }
 export default Layout
