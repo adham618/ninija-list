58eb19c0fa0a63a60783ae08bcf3ddabdb9ed58c
adham618 <adhamtarek291@gmail.com>
2021-12-15 19:21:39 +0200

    fetch data

--- pages/ninjas/index.tsx
+++ pages/ninjas/index.tsx
@@ -1,8 +1,39 @@
-const List: React.FC = () => {
+import { NextPage } from "next"
+import styles from "../../styles/Ninjas.module.css"
+interface indexProps {
+  ninjas: any
+}
+
+const List: NextPage<indexProps> = ({ ninjas }) => {
   return (
     <div>
       <h1>Ninjas</h1>
+      {ninjas.map((ninja: any) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h2>
+              {ninja.name}
+            </h2>
+          </a>
+        </div>
+      ))}
     </div>
   )
 }
 export default List
+
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
+  const data = await res.json()
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { ninjas: data }
+  }
+}
