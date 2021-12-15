7f2a199f22f0f66afabc5f7418ac0370c3970e48
adham618 <adhamtarek291@gmail.com>
2021-12-15 16:54:33 +0200

    custom error page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next"
+import Link from "next/link"
+
+const NotFound: NextPage = () => {
+  return (
+    <div className="not-found">
+      <h1>Oooops...</h1>
+      <h2>That page cannot be found.</h2>
+      <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
+    </div>
+  )
+}
+export default NotFound
